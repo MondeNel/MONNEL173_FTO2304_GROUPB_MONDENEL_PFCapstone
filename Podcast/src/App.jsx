c276d0bff0894b8040c30f834361e5599078f24a
Monde Nel <mondenel1996@gmail.com>
2023-10-24 09:23:00 +0200

    Updated project setup for React 18 and React Router v6
    Made necessary adjustments in the code to use React 18, fixed import statements for createRoot, and configured routing with React Router v6. Also, resolved 'createRoot' warning and wrapped the application with 'BrowserRouter' and 'React.StrictMode' for improved development practices.

---
+++ Podcast/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './Main_components/Home';
+import SignUp from './Signup_components/SignUp';
+import Login from './Signup_components/Login';
+import Search from './Navbar_components/Search';
+
+/**
+ * Functional component representing the main application.
+ * This component sets up routing using React Router and defines the base URL as "/app".
+ * It renders a single route at the root path.
+ * @returns {JSX.Element} The JSX element representing the application.
+ */
+const App = () => {
+  return (
+
+    <Routes>
+      <Route path="/" element={<Home />} /> {/* Renders the Home component at the root path. */}
+      <Route path="/signup" element={<SignUp />} /> {/* Renders the SignUp component at /app/signup. */}
+      <Route path="/login" element={<Login />} /> {/* Renders the Login component at /app/login. */}
+      <Route path="/search" element={<Search />} /> {/* Renders the Search component at /app/search. */}
+    </Routes>
+
+  );
+};
+
+export default App;
