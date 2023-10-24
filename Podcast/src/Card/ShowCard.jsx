b62ea39ab6aad4f5c884a758a5cc99791b7b126d
Monde Nel <mondenel1996@gmail.com>
2023-10-24 10:36:49 +0200

    Added the showCard component, which will render and display the selected show. Also, imported the showCard component within the search component to allow the user to search for shows

---
+++ Podcast/src/Card/ShowCard.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import './Card.css';
+
+const ShowCard = ({ show, onToggleFavorite }) => {
+    const [isDialogOpen, setDialogOpen] = useState(false);
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    const openDialog = () => {
+        setDialogOpen(true);
+    };
+
+    const closeDialog = () => {
+        setDialogOpen(false);
+    };
+
+    const toggleFavorite = () => {
+        setIsFavorite((prevIsFavorite) => !prevIsFavorite);
+        onToggleFavorite(show);
+    };
+
+    return (
+        <div className={`show-card ${isFavorite ? 'favorite' : ''}`}>
+            <img src={show.image} alt={show.title} />
+            <h2>{show.title}</h2>
+            <button onClick={openDialog}>Show Details</button>
+
+            <div className={`favorite-icon ${isFavorite ? 'favorite' : ''}`} onClick={toggleFavorite}>
+                {isFavorite ? <FavoriteIcon style={{ color: 'red' }} /> : <FavoriteIcon style={{ color: 'grey' }} />}
+            </div>
+
+            {isDialogOpen && (
+                <div className="dialog">
+                    <h3>{show.title}</h3>
+                    <p>{show.description}</p>
+                    <p>Number of Seasons: {show.seasons || 'N/A'}</p>
+                    <p>Year: {show.updated}</p>
+                    {/* Add genres and other show information as needed */}
+                    <button onClick={closeDialog}>Close</button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ShowCard;
--- Podcast/src/Navbar_components/Search.jsx
+++ Podcast/src/Navbar_components/Search.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import ShowCard from '../Card/ShowCard';
+import './Navbar.css';
 
 const API_URL = 'https://podcast-api.netlify.app/shows';
 
@@ -6,6 +9,9 @@ function Search() {
     const [searchInput, setSearchInput] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    // State to control the display of the overlay dialog
+    const [selectedShow, setSelectedShow] = useState(null);
 
     useEffect(() => {
         if (searchInput) {
@@ -30,23 +36,48 @@ function Search() {
         setSearchInput(e.target.value);
     };
 
+    // Function to open the dialog with selected show information
+    const showOverlayDialog = (show) => {
+        console.log('Selected Show:', show); // Console log the selected show
+        setSelectedShow(show);
+    };
+
+    // Function to close the dialog
+    const closeOverlayDialog = () => {
+        setSelectedShow(null);
+    };
+
     return (
-        <div>
-            <input
-                type="text"
-                placeholder="Search by title..."
-                value={searchInput}
-                onChange={handleInputChange}
-            />
+        <div className="search_bar">
+            <div className="search_bar_input">
+                <SearchIcon className="search__icon" />
+                <input
+                    type="text"
+                    placeholder="Search by title..."
+                    value={searchInput}
+                    onChange={handleInputChange}
+                />
+            </div>
+
             {loading && <p>Loading...</p>}
             {!loading && searchResults.length === 0 && searchInput !== '' && (
                 <p>No results found.</p>
             )}
             <ul>
                 {searchResults.map((result) => (
-                    <li key={result.id}>{result.title}</li>
+                    <li key={result.id} onClick={() => showOverlayDialog(result)}>
+                        {result.title}
+                    </li>
                 ))}
             </ul>
+
+            {/* ShowCard dialog */}
+            {selectedShow && (
+                <ShowCard
+                    show={selectedShow}
+                    onCloseDialog={closeOverlayDialog}
+                />
+            )}
         </div>
     );
 }
