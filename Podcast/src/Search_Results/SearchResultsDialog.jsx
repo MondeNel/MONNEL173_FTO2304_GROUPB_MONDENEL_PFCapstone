54e48bff6ba0504b9695771864fca0726fd75cdc
Monde Nel <mondenel1996@gmail.com>
2023-10-27 19:23:35 +0200

    Removed most of the information, because I just want the user to see the title and read about the show

--- Podcast/src/Search_Results/SearchResultsDialog.jsx
+++ Podcast/src/Search_Results/SearchResultsDialog.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import './Search.css';
 
 const SearchResultsDialog = ({ show, onClose }) => {
-    const maxLength = 420; // Adjust this as needed
-
-    const seasons = Array.from({ length: show.seasons }, (_, index) => index + 1);
-
-    // Assuming show.episodes is an array of episodes for all seasons
-    const episodes = show.episodes || [];
+    const maxLength = 420;
 
     return (
         <div className="modal-overlay">
@@ -23,42 +18,14 @@ const SearchResultsDialog = ({ show, onClose }) => {
                     </p>
                 </div>
 
-                <div className="dropdown">
-                    <button className="dropbtn">Seasons</button>
-                    <div className="dropdown-content">
-                        {seasons.map((season) => (
-                            <a key={season} href="#">
-                                Season {season}
-                            </a>
-                        ))}
-                    </div>
+
+                {/* Close the modal with the "Close" button */}
+                <div className="close-button">
+                    <button className="close-button__button" onClick={onClose}>Close</button>
                 </div>
 
-                <div className="dropdown">
-                    <button className="dropbtn">Episodes</button>
-                    <div className="dropdown-content">
-                        {episodes.map((episode) => (
-                            <a key={episode.id} href="#">
-                                {episode.title}
-                            </a>
-                        ))}
-                    </div>
-                </div>
             </div>
 
-            {/* Close the modal with the "Close" button */}
-            <div className="close-button">
-                <button className="close-button__button" onClick={onClose}>Close</button>
-            </div>
-
-            <div className="episodes-list">
-
-                <ul>
-                    {episodes.map((episode) => (
-                        <li key={episode.id}>{episode.title}</li>
-                    ))}
-                </ul>
-            </div>
         </div>
     );
 };
